186533f9222f8194a05d88f83adf54d1916c1373
Jiajun Jiao <jiaojiajunqd@gmail.com>
2023-10-15 15:19:12 +0000

    optimized Faker

--- tests/factory.py
+++ tests/factory.py
@@ -16,10 +16,10 @@ class PromotionFactory(factory.Factory):
         model = Promotion
 
     id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
+    name = factory.Faker("catch_phrase")
     code = factory.Faker("uuid4")
     start = FuzzyDate(date.today() - timedelta(days=30))
     expired = FuzzyDate(date.today(), date.today() + timedelta(days=30))
     whole_store = FuzzyChoice(choices=[True, False])
-    promo_type = factory.Sequence(lambda n: n % 5)  # assuming there are 5 types of promotions
-    value = FuzzyDecimal(0.1, 100.0)
+    promo_type = factory.Sequence(lambda n: n % 5)  # assuming 5 types of promotions
+    value = FuzzyDecimal(0.1, 100.0) # assuming value ranges from 0.1 to 100
