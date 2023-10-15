a0beb18c1dfa0bcb443cc3a36628fe24605c1d94
Jiajun Jiao <jiaojiajunqd@gmail.com>
2023-10-15 15:09:07 +0000

    Create factory.py and import it in test_models.py

---
+++ tests/factory.py
@@ -0,0 +1,25 @@
+"""
+Test Factory to make fake objects for testing
+"""
+from datetime import date, timedelta
+
+import factory
+from factory.fuzzy import FuzzyChoice, FuzzyDate, FuzzyDecimal
+from service.models import Promotion
+
+
+class PromotionFactory(factory.Factory):
+    """Creates fake promotions"""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Maps factory to data model"""
+        model = Promotion
+
+    id = factory.Sequence(lambda n: n)
+    name = factory.Faker("first_name")
+    code = factory.Faker("uuid4")
+    start = FuzzyDate(date.today() - timedelta(days=30))
+    expired = FuzzyDate(date.today(), date.today() + timedelta(days=30))
+    whole_store = FuzzyChoice(choices=[True, False])
+    promo_type = factory.Sequence(lambda n: n % 5)  # assuming there are 5 types of promotions
+    value = FuzzyDecimal(0.1, 100.0)
