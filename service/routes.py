0cdb0f3c07191df8dd0229e51193879b60df7080
Jiajun Jiao <97413019+Jiajun-Jiao@users.noreply.github.com>
2023-10-18 15:13:11 -0400

    Read(get_by_id) (#28)
    * Wrote read(get_by_id)
    
    * Updated default route
    
    * Updated README.md

--- service/routes.py
+++ service/routes.py
@@ -19,8 +19,13 @@ from . import app
 @app.route("/")
 def index():
     """Root URL response"""
+    app.logger.info("Request for Root URL")
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            name="Promotion Demo REST API Service",
+            version="1.0",
+            paths=url_for("list_promotions", _external=True),
+        ),
         status.HTTP_200_OK,
     )
 
@@ -132,3 +137,21 @@ def list_promotions():
     results = [p.serialize() for p in promotions]
     app.logger.info("Returning %d promotions", len(results))
     return jsonify(results), status.HTTP_200_OK
+
+######################################################################
+# RETRIEVE A PROMOTION
+######################################################################
+@app.route("/promotions/<int:promotion_id>", methods=["GET"])
+def get_promotions(promotion_id):
+    """
+    Retrieve a single Promotion
+
+    This endpoint will return a Promotion based on it's id
+    """
+    app.logger.info("Request for promotion with id: %s", promotion_id)
+    promotion = Promotion.find(promotion_id)
+    if not promotion:
+        abort(status.HTTP_404_NOT_FOUND, f"Promotion with id '{promotion_id}' was not found.")
+
+    app.logger.info("Returning promotion: %s", promotion.name)
+    return jsonify(promotion.serialize()), status.HTTP_200_OK
